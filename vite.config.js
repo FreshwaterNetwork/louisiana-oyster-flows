3c7518fccb73fa8b69593429c84d0c3cf1a1cf74
Casey Schneebeck <cschneebeck@tnc.org>
2024-01-18 08:58:24 -0700

    updated build path for dev site

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/oyster-flows/',
+  base: '/dev/oyster-flows/',
   plugins: [vue()],
   resolve: {
     alias: {
